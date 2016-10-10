c4c67485857cba13c756c58e0b0046e8d14662b2
Jay-Y <570440569@qq.com>
2016-10-10 23:48:20 +0800

    完善Template Servlet配置

--- template/template-webapp/src/main/java/modules/template/controller/base/TmpBaseController.java
+++ template/template-webapp/src/main/java/modules/template/controller/base/TmpBaseController.java
@@ -1,9 +1,6 @@
 package modules.template.controller.base;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.ui.Model;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.yserver.core.config.Constant;
 import org.yserver.core.controller.BaseController;
 
 /**
@@ -14,50 +11,4 @@ import org.yserver.core.controller.BaseController;
 public class TmpBaseController extends BaseController {
     @Value("${adminPath}")
     protected String adminPath;
-
-    /**
-     * 添加Model消息
-     *
-     * @param model
-     * @param messages
-     */
-    protected void addMessage(Model model, String... messages) {
-        StringBuilder sb = new StringBuilder();
-        for (String message : messages) {
-            sb.append(message).append(messages.length > 1 ? "<br/>" : "");
-        }
-        model.addAttribute(Constant.ServerConfig.RESPONSE_MSG, sb.toString());
-    }
-
-    /**
-     * 添加Model消息
-     *
-     * @param model
-     * @param status
-     * @param messages
-     */
-    protected void addMessage(Model model, char status, String... messages) {
-        model.addAttribute(Constant.ServerConfig.RESPONSE_STATUS, status);
-        StringBuilder sb = new StringBuilder();
-        for (String message : messages) {
-            sb.append(message).append(messages.length > 1 ? "<br/>" : "");
-        }
-        model.addAttribute(Constant.ServerConfig.RESPONSE_MSG, sb.toString());
-    }
-
-    /**
-     * 添加Flash消息
-     *
-     * @param redirectAttributes
-     * @param status
-     * @param messages
-     */
-    protected void addMessage(RedirectAttributes redirectAttributes, char status, String... messages) {
-        redirectAttributes.addFlashAttribute(Constant.ServerConfig.RESPONSE_STATUS, status);
-        StringBuilder sb = new StringBuilder();
-        for (String message : messages) {
-            sb.append(message).append(messages.length > 1 ? "<br/>" : "");
-        }
-        redirectAttributes.addFlashAttribute(Constant.ServerConfig.RESPONSE_MSG, sb.toString());
-    }
 }
